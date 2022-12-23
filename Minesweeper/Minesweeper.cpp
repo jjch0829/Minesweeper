baa8e55046531aa61144b20a0a38ccba021156ad
jjch0829 <121138458+jjch0829@users.noreply.github.com>
2022-12-23 21:33:58 +0900

    Update Minesweeper.cpp

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,49 +1,70 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
+#include <conio.h>
 
-void Judge_Num(int a[32][32], int b[32][32]) //지뢰 개수 판단 함수
+/*#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define ARROW 224
+
+void GotoXY(int x, int y) {
+    COORD Pos;
+    Pos.X = x;
+    Pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+void CursorView(char show)
 {
-    for (int i = 1; i < 31; i++)
-    {
-        for (int p = 1; p < 31; p++)
-        {
-            if (a[i][p] == 100)
-            {
-                int sum = 0;
-                for (int j = -1; j < 2; j++)
-                {
-                    if (a[i + 1][p + j] == 1)
-                    {
-                        sum += 1;
-                    }
-                }
-                if (a[i][p - 1] == 1)
-                {
-                    sum += 1;
-                }
-                if (a[i][p + 1] == 1)
-                {
-                    sum += 1;
-                }
-                for (int j = -1; j < 2; j++)
-                {
-                    if (a[i - 1][p + j] == 1)
-                    {
-                        sum += 1;
-                    }
-                }
-                a[i][p] += sum;
-                if (a[i][p] == 100)
-                {
-                   // b[i][p] = 1;
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+    ConsoleCursor.bVisible = show;
+    ConsoleCursor.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
+}
+int Move_Cursor() {
+
+    int x = 30;
+    int y = 7;
+    CursorView(0);
+    GotoXY(x, y);
+    printf("★");
+    while (1) {
+        if (_kbhit()) {
+            int nkey = _getch();
+            if (nkey == ARROW) {
+                nkey = _getch();
+                switch (nkey) {
+                case UP:
+                    system("cls");
+                    GotoXY(x, --y);
+                    printf("★");
+                    break;
+                case LEFT:
+                    system("cls");
+                    GotoXY(x = x - 2, y);
+                    printf("★");
+                    break;
+                case RIGHT:
+                    system("cls");
+                    GotoXY(x = x + 2, y);
+                    printf("★");
+                    break;
+                case DOWN:
+                    system("cls");
+                    GotoXY(x, ++y);
+                    printf("★");
+                    break;
+
                 }
             }
         }
     }
-}
 
+    return 0;
+}
+*/
 void User_Printing(int a[32][32], int b[32][32]) //프린트 메인 함수
 {
     for (int i = 1; i < 31; i++)
@@ -125,6 +146,162 @@ void User_Printing(int a[32][32], int b[32][32]) //프린트 메인 함수
     }
 }
 
+void Boom(int a[32][32], int b[32][32]) //지뢰가 터졌을시 사용함수
+{
+    for (int i = 1; i < 31; i++)
+    {
+        for (int p = 1; p < 31; p++)
+        {
+            b[i][p] = 1;
+        }
+    }
+    User_Printing(b, a);
+}
+/*
+int key_Board(int map[32][32], int checking[32][32], int Printing[32][32])
+{
+    int c, d;
+    for (;;)
+    {
+        if (_kbhit())
+        {        //키보드 입력 확인 (true / false)
+            c = _getch();      // 방향키 입력시 224 00이 들어오게 되기에 앞에 있는 값 224를 없앰
+            if (c == -32)
+            {    // -32로 입력되면
+                c = _getch();  // 새로 입력값을 판별하여 상하좌우 출력
+                switch (c)
+                {
+                case LEFT:
+                    //printf("왼쪽으로 이동\n");
+                    c--;
+                    break;
+                case RIGHT:
+                    //printf("오른쪽으로 이동\n");
+                    c++;
+                    break;
+                case UP:
+                    //printf("위로 이동\n");
+                    d++;
+                    break;
+                case DOWN:
+                    //printf("아래로 이동\n");
+                    c--;
+                    break;
+                }
+            }
+        }
+    }
+    if (c >= 31 || d >= 31 || c < 1 || d < 1)
+    {
+        printf("no range\n");
+        Sleep(1000);
+    }
+    else if (map[c][d] == 1)
+    {
+        checking[c][d] = 1;
+        system("cls");
+        printf("Boom!!\n");
+        printf("Bomb location\n");
+        Boom(map, checking);
+        return 0;
+    }
+    else if (checking[c][d] == 1)
+    {
+        printf("already checked\n");
+        return 1;
+    }
+    else if (map[c][d] >= 100)
+    {
+        printf("Safed location\n");
+        checking[c][d] = 1;
+        return 1;
+    }
+
+    //Sleep(1000);
+    system("cls");
+    return 1;
+}
+*/
+int User_Input(int map[32][32], int checking[32][32], int Printing[32][32]) //입력 메인함수
+{
+    int c, d;
+    printf("x location input: ");
+    scanf_s("%d", &c);
+    printf("y location input: ");
+    scanf_s("%d", &d);
+
+    if (c >= 31 || d >= 31 || c < 1 || d < 1)
+    {
+        printf("no range\n");
+        Sleep(1000);
+    }
+    else if (map[c][d] == 1)
+    {
+        checking[c][d] = 1;
+        system("cls");
+        printf("Boom!!\n");
+        printf("Bomb location\n");
+        Boom(map, checking);
+        return 0;
+    }
+    else if (checking[c][d] == 1)
+    {
+        printf("already checked\n");
+        return 1;
+    }
+    else if (map[c][d] >= 100)
+    {
+        printf("Safed location\n");
+        checking[c][d] = 1;
+        return 1;
+    }
+
+    //Sleep(1000);
+    system("cls");
+    return 1;
+}
+
+void Judge_Num(int a[32][32], int b[32][32]) //지뢰 개수 판단 함수
+{
+    for (int i = 1; i < 31; i++)
+    {
+        for (int p = 1; p < 31; p++)
+        {
+            if (a[i][p] == 100)
+            {
+                int sum = 0;
+                for (int j = -1; j < 2; j++)
+                {
+                    if (a[i + 1][p + j] == 1)
+                    {
+                        sum += 1;
+                    }
+                }
+                if (a[i][p - 1] == 1)
+                {
+                    sum += 1;
+                }
+                if (a[i][p + 1] == 1)
+                {
+                    sum += 1;
+                }
+                for (int j = -1; j < 2; j++)
+                {
+                    if (a[i - 1][p + j] == 1)
+                    {
+                        sum += 1;
+                    }
+                }
+                a[i][p] += sum;
+                if (a[i][p] == 100)
+                {
+                    b[i][p] = 1;
+                }
+            }
+        }
+    }
+}
+
 void ini_set(int map[32][32], int Printing[32][32], int checking[32][32])
 {
     srand(time(NULL));
@@ -168,20 +345,8 @@ void ini_set(int map[32][32], int Printing[32][32], int checking[32][32])
         Printing[a][b] = 1;
     }
 
-    Judge_Num(map,checking);
+    Judge_Num(map, checking);
 
-}
-
-void Boom(int a[32][32], int b[32][32])
-{
-    for (int i = 1; i < 31; i++)
-    {
-        for (int p = 1; p < 31; p++)
-        {
-            b[i][p] = 1;
-        }
-    }
-    User_Printing(b, a);
 }
 
 int main()
@@ -189,46 +354,30 @@ int main()
     int map[32][32];
     int Printing[32][32];
     int checking[32][32];
-    int wd = 1;
-    int c, d;
-    ini_set(map, Printing, checking);
+    int wd = 1, game = 1; //초기 변수 선언
 
-    while (wd == 1)
+    while (wd == 1) // 프로그램 종료 여부
     {
-        User_Printing(checking, map);
-        printf("x location input: ");
-        scanf_s("%d", &c);
-        printf("y location input: ");
-        scanf_s("%d", &d);
-
-        if (c >= 31 || d >= 31 || c < 1 || d < 1)
+        ini_set(map, Printing, checking);
+        while (game == 1) //게임 승리 여부 
         {
-            printf("no range\n");
-        }
-        else if (map[c][d] == 1)
-        {
-            int any;
-            checking[c][d] = 1;
             system("cls");
-            printf("Boom!!\n");
-            printf("Bomb location\n");
-            Boom(map, checking);
-            scanf_s("%d", &any);
+            User_Printing(checking, map);
+            game = User_Input(map, checking, Printing);
+            wd = game;
+            Sleep(1000);
+        }
+        /*int ans;
+        Sleep(10000);
+        printf("Do You Wanna Play Game More? \n");
+        printf("If You Want Input 'Y' : \n");
+        scanf("%d", &ans);
+        if (ans == 1)
+        {
             wd = 0;
-        }
-        else if (checking[c][d] == 1)
-        {
-            printf("already checked\n");
-        }
-        else if (map[c][d] >= 100)
-        {
-            printf("Safed location\n");
-            checking[c][d] = 1;
-        }
-
-        //Sleep(1000);
-        system("cls");
+            printf("Thanks For Playing Game");
+        }*/
     }
-    printf("Bomed location\n");
-
 }
+
+
